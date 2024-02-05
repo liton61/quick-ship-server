bbb44edbdc81520ca83c6be66bdd295b1cf4dd62
Shah Alam <shahalam01660166344@gmail.com>
2024-02-05 17:45:20 +0600

    added admin

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const stripe = require('stripe')("sk_test_51OEHKpItrEdLuT7QFfLsHZW8zwDrTSOHDcz96uSkTmnuQ53TLekmLbYUrGmo3jziXO1Y16TUy8uQPmrSBNU3udx700F2sQLM6W")
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
@@ -10,12 +10,10 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
-console.log(process.env.DB_PASS);
 
-//quickshipUser
 
-//nw2J9VqXcQ8zWX1N
-const uri = `mongodb+srv://quickshipUser:nw2J9VqXcQ8zWX1N@cluster0.dbdkno8.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dbdkno8.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,14 +28,102 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+    const userCollection = client.db("quickship").collection("users");
     const pricingCollection = client.db("quickship").collection("pricing");
     const orderCollection = client.db("quickship").collection("order");
     const paymentCollection = client.db("quickship").collection("payment");
-    const calculatorCollection = client.db("quickship").collection("calculator");
+    const calculatorCollection = client
+      .db("quickship")
+      .collection("calculator");
+
+    // jwt related api
+    // app.post('/jwt', async (req, res) => {
+    //   const user = req.body;
+    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+    //   res.send({ token });
+    // })
+
+    // // middlewares
+    // const verifyToken = (req, res, next) => {
+    //   // console.log('inside verify token', req.headers.authorization);
+    //   if (!req.headers.authorization) {
+    //     return res.status(401).send({ message: 'unauthorized access' });
+    //   }
+    //   const token = req.headers.authorization.split(' ')[1];
+    //   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    //     if (err) {
+    //       return res.status(401).send({ message: 'unauthorized access' })
+    //     }
+    //     req.decoded = decoded;
+    //     next();
+    //   })
+    // }
+
+    // use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+
+    // users related api
+    app.get("/users", verifyAdmin, async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // app.patch("/users/admin/:id", verifyAdmin, async (req, res) => {
+    //   const id = req.params.id;
+    //   const filter = { _id: new ObjectId(id) };
+    //   const updatedDoc = {
+    //     $set: {
+    //       role: "admin",
+    //     },
+    //   };
+    //   const result = await userCollection.updateOne(filter, updatedDoc);
+    //   res.send(result);
+    // });
+
+    app.delete("/users/:id", verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
 
     //pricing collection
-    // DB_USER=quickshipUser
-    // DB_PASS=nw2J9VqXcQ8zWX1N
     app.get("/price-box", async (req, res) => {
       const result = await pricingCollection.find().toArray();
       res.send(result);
@@ -52,48 +138,46 @@ async function run() {
       res.send(result);
     });
 
-    // Order collection 
+    // Order collection
     app.get("/order", async (req, res) => {
-      const result = await orderCollection.find().toArray()
-      res.send(result)
-    })
+      const result = await orderCollection.find().toArray();
+      res.send(result);
+    });
 
     app.post("/order", async (req, res) => {
-      const order = req.body
+      const order = req.body;
       order.time = new Date();
       // console.log(order);
-      const result = await orderCollection.insertOne(order)
-      res.send(result)
-    })
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
 
     //payment api here
     app.post("/create-payment-intent", async (req, res) => {
       try {
-        const {
-          amount
-        } = req.body
+        const { amount } = req.body;
 
-        const totalAmount = parseFloat(amount * 100)
+        const totalAmount = parseFloat(amount * 100);
         // console.log(totalAmount);
         const paymentIntent = await stripe.paymentIntents.create({
           amount: totalAmount,
           currency: "usd",
-          payment_method_types: ["card"]
-        })
+          payment_method_types: ["card"],
+        });
 
         res.send({
-          clientSecret: paymentIntent.client_secret
-        })
+          clientSecret: paymentIntent.client_secret,
+        });
       } catch (error) {
         console.log(error.message);
       }
-    })
+    });
 
     app.post("/payment", async (req, res) => {
-      const payment = req.body
-      const result = await paymentCollection.insertOne(payment)
-      res.send(result)
-    })
+      const payment = req.body;
+      const result = await paymentCollection.insertOne(payment);
+      res.send(result);
+    });
 
     app.get("/payment", async (req, res) => {
       // const user = req.query.email
@@ -101,28 +185,24 @@ async function run() {
       // if (user) {
       //     query.email = user;
       // }
-      const result = await paymentCollection.find().toArray()
-      res.send(result)
-    })
-
-
+      const result = await paymentCollection.find().toArray();
+      res.send(result);
+    });
 
     // Delivery Calculator get here
-    app.get("/calculator", async(req, res) =>{
-      const result = await calculatorCollection.find().toArray()
-      res.send(result)
-    })
+    app.get("/calculator", async (req, res) => {
+      const result = await calculatorCollection.find().toArray();
+      res.send(result);
+    });
 
     // Delivery Calculator post here
-    app.post("/calculator", async (req, res) =>{
-      const calculator = req.body
+    app.post("/calculator", async (req, res) => {
+      const calculator = req.body;
       calculator.time = new Date();
       // console.log(calculator);
-      const result = await calculatorCollection.insertOne(calculator)
-      res.send(result)
-    })
-
-
+      const result = await calculatorCollection.insertOne(calculator);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });

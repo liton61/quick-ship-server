a12fec090d9db39b4e5f9c202e9055178b644d01
Airin Rahat Prome <airinrahatprome@gmail.com>
2024-02-22 12:23:09 +0600

    get id ways services data

--- index.js
+++ index.js
@@ -114,6 +114,14 @@ async function run() {
       res.send(result);
     });
 
+    //get service data id ways
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
+
     // =========================== APPLICATION ==============================
 
     // post method for application

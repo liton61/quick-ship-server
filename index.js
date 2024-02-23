d794badffced2288caa40115f99198b06b449f47
S H-SuHan <suhanur35-448@diu.edu.bd>
2024-02-23 20:00:54 +0600

    Blog data get single id  here

--- index.js
+++ index.js
@@ -131,7 +131,16 @@ async function run() {
           res.send(result);
         })
 
+        app.get("/posts/:id", async (req, res) => {
+          const id = req.params.id;
+          const query = {
+            _id: new ObjectId(id)
+          };
+          const result = await postsCollection.findOne(query);
+          res.send(result);
+        });
 
+         
     // =========================== APPLICATION ==============================
 
     // post method for application
